25fa84d65724dbf5afbd29f3c39e4df089bd5060
liboshi <827864735@qq.com>
2021-09-28 18:50:55 +0800

    22

--- .umirc.ts
+++ .umirc.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   title: 'whale',
   base: '/whale',
   publicPath: '/whale/',
+  exportStatic: {},
   favicon:
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
