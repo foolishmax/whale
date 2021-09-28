438c39a96cfd49c8598de95a5491db4cc4589683
liboshi <827864735@qq.com>
2021-09-28 18:29:04 +0800

    Updates

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var React = require('react');
+
+function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+
+var React__default = /*#__PURE__*/_interopDefaultLegacy(React);
+
+var index = (function (_ref) {
+  var title = _ref.title;
+  return /*#__PURE__*/React__default['default'].createElement("h1", null, "666", title);
+});
+
+var index$1 = (function (_ref) {
+  var title = _ref.title;
+  return /*#__PURE__*/React__default['default'].createElement("h1", null, "111", title);
+});
+
+exports.Button = index;
+exports.Input = index$1;
